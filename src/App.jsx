f3f0b361ad3b70eb9839e9da72b15d87722f19e4
Sohail025 <msohail0264@gmail.com>
2024-07-29 15:18:12 +0500

    Commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Login from "./Pages/Login";
-// import Home from "./Pages/Home";
-// import SentRequests from "./Pages/SentRequests";
-// import ReceivedRequests from "./Pages/ReceivedRequests";
-// import Product from "./Pages/Product";
-// import Profile from "./Pages/Profile";
+import Home from "./Pages/Home";
+import SentRequests from "./Pages/SentRequests";
+import ReceivedRequests from "./Pages/ReceivedRequests";
+import Product from "./Pages/Product";
+import Profile from "./Pages/Profile";
 import AddProduct from "./Pages/AddProduct";
-const router = createBrowserRouter([
-  { path: "/login", element: <Login /> },
-  { path: "addproduct", element: <AddProduct /> },
-]);
+// const router = createBrowserRouter([
+//   { path: "/login", element: <Login /> },
+//   { path: "addproduct", element: <AddProduct /> },
+// ]);
 export const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <div>
+      <Login />
+      <Home />
+      <AddProduct />
+      <Profile />
+      <Product />
+      <SentRequests />
+      <ReceivedRequests />
+    </div>
+  );
+  // <RouterProvider router={router} />;
 };
 export default App;
