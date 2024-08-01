9749dc79a08accb9b065a4f3c7a2fc2966edd865
Duraid <m.duraid551@gmail.com>
2024-08-01 18:18:00 +0500

    Created Adding Products API

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { createUser, login } = require("../controllers/post");
+const { createUser, login, addProduct } = require("../controllers/post");
 // const { getAllItems, GetSingleItem } = require("../controllers/get");
 // const {
 //   deleteAllItems,
@@ -9,19 +9,8 @@ const { createUser, login } = require("../controllers/post");
 //   deleteAllCart,
 // } = require("../controllers/delete");
 
-// router.post("/add", addItems);
-// router.post("/form", contact_us);
-// router.post("/check/user", CheckUser);
-// router.post("/add/in/cart", addItemsInCart);
 router.post("/createUser", createUser);
 router.post("/login", login);
-// router.post("/add/in/purchase", addItemsInPurchase);
-// router.post("/get/all/cart", getAllCartItems);
-// router.get("/get/all", getAllItems);
-// router.post("/get/all/purchase", getAllPurchaseItems);
-// router.get("/get/single/:id", GetSingleItem);
-// router.delete("/delete/all", deleteAllItems);
-// router.delete("/delete/single", deleteSingleItem);
-// router.delete("/delete/single/cart", deleteSingleCart);
-// router.delete("/delete/all/cart", deleteAllCart);
+router.post("/addProduct", addProduct);
+
 module.exports = router;
